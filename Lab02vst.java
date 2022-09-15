ccb9d3d19eb562d2ec4b310f0fef98002f6cb955
Ryan monesmith <ryan.monesmith@vtsd.com>
2022-09-15 08:10:18 -0400

    Lab done

--- Lab02vst.java
+++ Lab02vst.java
@@ -8,7 +8,22 @@
 
   public class Lab02vst {
 
-    public static void main(String args[]){
+    public static void main(String[] args)
+    {
+      System.out.println("Lab 02, 80 Point Version");
+      System.out.println();
+      System.out.println();
+      System.out.print("Measure today's knowledge ");
+      System.out.println("by yesterday's confusion");
+      System.out.println();
+      System.out.println("Bewilderment + Exposure = obvious");
+      System.out.println();
+      System.out.println("# ##### #####    ##### ##### # #    # ##### #####");
+      System.out.println("# #   # #   #    #   # #   # # # #  #   #   #");
+      System.out.println("# #   # #   #    ##### #   # # #  # #   #   #####");
+      System.out.println("# #   # #   #    #     #   # # #   ##   #       #");
+      System.out.println("# ##### #####    #     ##### # #    #   #   #####");
+
 
     }
   }
